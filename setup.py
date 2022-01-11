ccb0b620cdc243eb1652d58d370203a4910d92a7
masinag <gabriele.masina@virgilio.it>
2022-01-11 15:16:50 +0100

    Add mypy support

--- setup.py
+++ setup.py
@@ -4,28 +4,21 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     # This is the name of your project. The first time you publish this
-    name='kad-simulator',  # Required
-
+    name="kad-simulator",  # Required
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version='0.0.1',  # Required
-
+    version="0.0.1",  # Required
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Kademlia simulator',  # Optional
-
+    description="Kademlia simulator",  # Optional
     # https://packaging.python.org/specifications/core-metadata/#description-optional
     long_description=long_description,  # Optional
-
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
+    long_description_content_type="text/markdown",  # Optional (see note above)
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/GiovanniZotta/kademlia',  # Optional
-
-    author='Gabriele Masina, Alex Zanetti, Giovanni Zotta',  # Optional
-
+    url="https://github.com/GiovanniZotta/kademlia",  # Optional
+    author="Gabriele Masina, Alex Zanetti, Giovanni Zotta",  # Optional
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see https://pypi.org/classifiers/
@@ -34,39 +27,33 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Distributed Systems :: Simulators :: Kademlia',
-
+        "Intended Audience :: Developers",
+        "Topic :: Distributed Systems :: Simulators :: Kademlia",
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        'Programming Language :: Python :: 3 :: Only',
+        "Programming Language :: Python :: 3 :: Only",
     ],
-
     # This field adds keywords for your project which will appear on the
     # project page. What does your project relate to?
     #
     # Note that this is a list of additional keywords, separated
     # by commas, to be used to assist searching for the distribution in a
     # larger catalog.
-    keywords='distributed systems, simulator, kademlia',  # Optional
-
+    keywords="distributed systems, simulator, kademlia",  # Optional
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={'': 'src'},  # Optional
-
+    package_dir={"": "src"},  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -76,14 +63,12 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where='src'),  # Required
-
+    packages=find_packages(where="src"),  # Required
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.6, <4',
-
+    python_requires=">=3.6, <4",
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
@@ -91,7 +76,6 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
     # install_requires=['peppercorn'],  # Optional
-
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
@@ -104,20 +88,18 @@ setup(
         # 'dev': ['check-manifest'],
         # 'test': ['coverage'],
     },
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     package_data={  # Optional
         # 'sample': ['package_data.dat'],
+        "kad": ["py.typed"]
     },
-
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/distutils/setupscript.html#installing-additional-files
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],  # Optional
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
@@ -130,7 +112,6 @@ setup(
         #     'sample=sample:main',
         # ],
     },
-
     # List additional URLs that are relevant to your project as a dict.
     #
     # This field corresponds to the "Project-URL" metadata fields:
@@ -141,9 +122,10 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/GiovanniZotta/kademlia/issues',
+        "Bug Reports": "https://github.com/GiovanniZotta/kademlia/issues",
         # 'Funding': 'https://donate.pypi.org',
         # 'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/pypa/kademlia/',
+        "Source": "https://github.com/pypa/kademlia/",
     },
+    zip_safe=False,
 )
