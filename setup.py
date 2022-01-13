a86a9f54423dafc6d81c84aaaa3703259ae31335
Giovanni Zotta <giovannizotta771@gmail.com>
2022-01-13 09:56:24 +0100

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -75,7 +75,10 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    # install_requires=['peppercorn'],  # Optional
+    install_requires=[
+        'simpy>=4',
+        'numpy>=1.22'
+    ],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
