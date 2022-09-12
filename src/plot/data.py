5bb456ff13ddd984256e2acc04d1cf2ff287f434
Gabriele Masina <gabriele.masina@virgilio.it>
2022-09-12 16:10:15 +0200

    Bugfix6(?)

--- src/plot/data.py
+++ src/plot/data.py
@@ -129,12 +129,12 @@ def get_slots_with_ci(df: dd.DataFrame, slot_column: str, metric: str, slot_agg:
             metric: slot_agg,
             "slot_metric_count": "count",
         }).compute().unstack().stack(dropna=False).reset_index()
-    df["slot_metric"] = df.groupby("seed")[slot_agg].ffill().fillna(0)
+    df["slot_metric"] = df.groupby("seed")[metric].ffill().fillna(0)
 
     # foreach slot, compute the mean across seeds aggregate results with confidence intervals
     df = df.groupby(["slot"]).aggregate({
         "slot_metric": ["mean", "sem"],
-        "count": "sum",
+        "slot_metric_count": "sum",
     }).reset_index().fillna(0)
 
     df["slot"] *= slot_width
